1946769a839f94195d1b6cb7195732bd0e1f6737
ELIYAHUT123 <eliyahut123@gmail.com>
2016-09-21 22:51:25 +0300

    Add "objectify" option
    I think it would be nice if there was option to get back an object instead of a list.
    
    Example:
    Let's say you running a food delivery company and you save in the DB the details of "meals" and "deliveries".
    The result would look something like this:
    ```
    [
        {
            "meals": [
                {
                    "id": 1,
                    "name": "meal1",
                    "description": "description of meal1",
                    "price": 200,
                },
                {
                    "id": 2,
                    "name": "meal2",
                    "description": "description of meal2",
                    "price": 250,
                }
            ]
        },
        {
            "deliveries": [
                {
                    "id": 1,
                    "address": "Somewhere",
                },
                {
                    "id": 2,
                    "address": "Somewhere else",
                }
            ]
       }
    ]
    ```
    With this kind of result you can't do something like:
    ```
    axios.get(url).then(response => {
        alert(response.data.meals)
    })
    ```
    Because response.data is an array.
    Instead you will have to iterate through the result and look for an object that has a key "meals".
    It's a shame because this is a very common use.
    
    In order to solve it I added is a little flag called "objectify" (a little like "flat").
    In case objectify=True you will get something like this:
    ```
    {
        "meals": [
                {
                    "id": 1,
                    "name": "meal1",
                    "description": "description of meal1",
                    "price": 200,
                },
                {
                    "id": 2,
                    "name": "meal2",
                    "description": "description of meal2",
                    "price": 250,
                }
            ]
            "deliveries": [
                {
                    "id": 1,
                    "address": "Somewhere",
                },
                {
                    "id": 2,
                    "address": "Somewhere else",
                }
            ]
    }
    ```
    Now you can easily do:
    ```
    axios.get(url).then(response => {
        alert(response.data.meals)
    })
    ```

--- drf_multiple_model/mixins.py
+++ drf_multiple_model/mixins.py
@@ -1,5 +1,9 @@
 from rest_framework.response import Response
 
+from itertools import chain
+
+from django.db import connection
+
 
 class MultipleModelMixin(object):
     """
@@ -15,8 +19,8 @@ class MultipleModelMixin(object):
             .....
     ]
 
-    optionally, you can add a third element to the queryList,
-    a label to define that particular data type:
+    optionally, you can add a third element to the queryList, a label to define that particular
+    data type:
 
     queryList = [
             (querysetA,serializerA,'labelA'),
@@ -26,6 +30,7 @@ class MultipleModelMixin(object):
     ]
 
     """
+    objectify = False
 
     queryList = None
 
@@ -50,26 +55,29 @@ class MultipleModelMixin(object):
 
         return queryList
 
-    def paginate_queryList(self, queryList):
+    def paginate_queryList(self,queryList):
         """
         Wrapper for pagination function.
-        By default it just calls paginate_queryset,
-        but can be overwritten for custom functionality
+        By default it just calls paginate_queryset, but can be overwritten for custom functionality
         """
-        return self.paginate_queryset(queryList)
+        return self.paginate_queryset(queryList) 
 
     def list(self, request, *args, **kwargs):
         queryList = self.get_queryList()
 
-        # Iterate through queryList, run each queryset and serialize the data
-        results = []
+        if self.objectify:
+            results = {}
+        else:
+            results = []
+
+        # Iterate through the queryList, run each queryset and serialize the data
         for query in queryList:
             if not isinstance(query, Query):
                 query = Query.new_from_tuple(query)
             # Run the queryset through Django Rest Framework filters
             queryset = query.queryset.all()
             queryset = self.filter_queryset(queryset)
-
+            
             # If there is a user-defined filter, run that too.
             if query.filter_fn is not None:
                 queryset = query.filter_fn(queryset, request, *args, **kwargs)
@@ -84,7 +92,7 @@ class MultipleModelMixin(object):
             # Sort by given attribute, if sorting_attribute is provided
             if self.sorting_field:
                 results = self.queryList_sort(results)
-
+            
             # Return paginated results if pagination is enabled
             page = self.paginate_queryList(results)
             if page is not None:
@@ -95,8 +103,8 @@ class MultipleModelMixin(object):
 
         return Response(results)
 
-    # formats the serialized data based on various view properties
-    def format_data(self, new_data, query, previous_results):
+    # formats the serialized data based on various view properties (e.g. flat=True)
+    def format_data(self, new_data, query, results):
         # Get the label, unless add_model_type is note set
         label = None
         if query.label is not None:
@@ -110,24 +118,29 @@ class MultipleModelMixin(object):
             for datum in new_data:
                 if label:
                     datum.update({'type': label})
-                previous_results.append(datum)
+                results.append(datum)
+
+        # if objectify=True, Organize the data in an object
+        elif self.objectify:
+            if not label:
+                raise RuntimeError("Cannot objectify data. Try to use objectify=False")
+
+            results[label] = new_data
 
         # Otherwise, group the data by Model/Queryset
         else:
             if label:
                 new_data = {label: new_data}
 
-            previous_results.append(new_data)
+            results.append(new_data)
 
-        return previous_results
+        return results
+
 
     # Sort based on the given sorting field property
     def queryList_sort(self, results):
-        """
-        determing if sort is ascending or descending
-        based on the presence of '-' at the beginning of the
-        sorting_field attribute
-        """
+        # determing if sort is ascending or descending based on the presence of '-' at the beginning of the
+        # sorting_field attribute
         sorting_field = self.sorting_field
         sort_descending = self.sorting_field[0] == '-'
 
@@ -135,15 +148,11 @@ class MultipleModelMixin(object):
         if sort_descending:
             sorting_field = sorting_field[1:len(sorting_field)]
 
-        return sorted(
-            results,
-            reverse=sort_descending,
-            key=lambda datum: datum[sorting_field]
-        )
+        return sorted(results, reverse=sort_descending, key=lambda datum: datum[sorting_field])
 
 
 class Query(object):
-    def __init__(self, queryset, serializer, label=None, filter_fn=None, ):
+    def __init__(self, queryset, serializer, label=None,filter_fn=None, ):
         self.queryset = queryset
         self.serializer = serializer
         self.filter_fn = filter_fn
